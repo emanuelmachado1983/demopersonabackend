70c17d0152c2bbcca365167219f2826454836d33
unknown <emanuelmachado77@gmail.com>
2021-01-03 12:20:32 -0300

    segundo

--- src/main/java/com/emanuel/persona/PersonaBackendApplication.java
+++ src/main/java/com/emanuel/persona/PersonaBackendApplication.java
@@ -16,7 +16,7 @@ public class PersonaBackendApplication {
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
-			public void addCorsMappings(CorsRegistry registry) {
+			public void addCorsMappings(CorsRegistry registry) { 
 				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("*").allowedHeaders("*");
 			}
 		};
